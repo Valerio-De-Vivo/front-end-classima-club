1f769e093fcf8d184ebdd0c3b7c92d264bde6b90
Valerio <valeriodevivo90@gmail.com>
2021-06-17 12:19:56 +0200

    Finish

--- sito/js/app.js
+++ sito/js/app.js
@@ -1,7 +1,20 @@
 $(document).foundation();
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 5,
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    1080: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    }
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -14,7 +27,20 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 var swiper = new Swiper(".mySwiper2", {
-  slidesPerView: 3,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1080: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
